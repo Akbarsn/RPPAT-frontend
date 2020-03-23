8d8a556d33ad428dfcba354364067ed5db2b6972
akbarsn <new.akbarsatya@gmail.com>
2020-03-23 19:58:47 +0700

    Added Homepage for outlet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import { Menu, Row, Col, Layout, Dropdown } from 'antd'
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./Navbar.scss";
 
+import { DownOutlined } from "@ant-design/icons";
 
 export default function Navbar() {
 
     const menu = (
         < Menu >
-            <Menu.Item key="editProfile" >
+            <Menu.Item key="editProfile" className="dropdownMenu">
                 <Link to='/edit-profile'>Ganti Profile</Link>
             </Menu.Item >
 
-            <Menu.Item key="logOut">
+            <Menu.Item key="logOut" className="dropdownMenu">
                 <Link to='/edit-profile'>Keluar</Link>
             </Menu.Item>
         </Menu >
     )
 
     return (
-        <Layout.Header>
-            <Row justify='end'>
-                <Col span={5}>
+        <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            <Row justify='space-between'>
+                <Col span={3}>
+                    <span id="navHeader">
+                        RPPAT
+                        </span>
+                </Col>
+                <Col span={3}>
                     <Dropdown overlay={menu}>
-                        <a id="greetItem" onClick={e => e.preventDefault()}>
-                            Hello Akbar
-                        </a>
+                        <Row align="middle" id="helloNav">
+                            Hello Akbar <DownOutlined style={{ fontSize: '24px', marginLeft: '24px' }} />
+                        </Row>
                     </Dropdown>
                 </Col>
             </Row>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,80 @@
 import React from 'react'
-import {Menu} from "antd";
+import { Menu, Layout, Avatar, Col, Row } from "antd";
+import "./Sidebar.scss"
 
-const {SubMenu, Item} = Menu
+import {
+    UserOutlined, BookOutlined, InfoCircleOutlined,
+    ShoppingCartOutlined, TransactionOutlined, UserAddOutlined
+}
+    from "@ant-design/icons";
+import SubMenu from 'antd/lib/menu/SubMenu';
+
 
 export default function Sidebar() {
     return (
-        <div>
-            
-        </div>
+        <Layout.Sider style={{
+            minHeight: '100vh',
+            position: 'fixed',
+            left: 0,
+        }} width='400' id='sidebar'>
+            <div id="photoProfile">
+                <Col className="center">
+                    <Avatar size={80} icon={<UserOutlined />} style={{ display: 'block' }} />
+                </Col>
+
+                <Col>
+                    <span className="header">Outlet</span>
+                </Col>
+
+                <div id="divider"></div>
+            </div>
+            <Menu
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                id="sidebar"
+            >
+
+                <SubMenu key="laporan" className="item"
+                    title={
+                        <Row align="middle">
+                            <BookOutlined className='primary' />
+                            <span className="primary text">Laporan</span>
+                        </Row>
+                    }>
+
+                    <Menu.Item className="secondary">Penjualan</Menu.Item>
+                    <Menu.Item className="secondary">Pembelian</Menu.Item>
+                </SubMenu>
+
+                <Menu.Item className='item primary'>
+                    <Row align="middle">
+                        <InfoCircleOutlined className="primary" />
+                        Lihat Stok
+                    </Row>
+                </Menu.Item>
+
+                <Menu.Item className="item primary">
+                    <Row align="middle">
+                        <ShoppingCartOutlined className="primary" />
+                        Beli Stok
+                    </Row>
+                </Menu.Item>
+
+                <Menu.Item className="item primary">
+                    <Row align="middle">
+                        <TransactionOutlined className="primary" />
+                        Riwayat Transaksi
+                    </Row>
+                </Menu.Item>
+
+                <Menu.Item className="item primary">
+                    <Row align="middle">
+                        <UserAddOutlined className="primary" />
+                        Tambah Kasir
+                    </Row>
+                </Menu.Item>
+            </Menu>
+        </Layout.Sider>
     )
 }
