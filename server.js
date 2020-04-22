616f38ef41a9b636df32d6c475c2e8d10fe61d4e
Akbar Nugraha <new.akbarsatya@gmail.com>
2020-04-23 04:44:19 +0700

    Fixing deployment

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(80, ()=>{
+app.listen(3000, ()=>{
     console.log("Listening to 9000")
 });
